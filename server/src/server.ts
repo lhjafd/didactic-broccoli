a69d33b24c341f8e1fb1a86b74f92e135083d4ac
Mccomack <135214055+Mccomack@users.noreply.github.com>
2026-06-01 18:18:50 +0900

    /api/test에 테스트용 응답 추가

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,11 @@
-console.log("Hello, World!");
+import express, {Request, Response} from "express";
+const app = express();
+const port = 3000;
+
+app.get('/api/test', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
