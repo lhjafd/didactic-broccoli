236f6572a95f0ed1c5e7c61b40c8ede11e967d8b
Hodu0917 <hongin0917@gmail.com>
2026-06-10 18:18:15 +0900

    음식 조합 페이지 추가

---
+++ web/src/foodcombo.js
@@ -0,0 +1,11 @@
+function toggleForm(){
+
+    const form = document.getElementById("comboForm");
+
+    if(form.style.display === "block"){
+        form.style.display = "none";
+    }else{
+        form.style.display = "block";
+    }
+
+}
